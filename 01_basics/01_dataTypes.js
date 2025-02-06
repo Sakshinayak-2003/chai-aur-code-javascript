0d8d33867b095533f7fcfbd9491805dffb3266bd
Sakshinayak-2003 <nayaksakshi949@gmail.com>
2025-02-06 19:45:09 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,15 @@
+  "use strict";         //TREAT ALL JS CODE AS NEWER VERSION
+  //alert("hello") // will not error give an error
+
+//   alert (3+3) // we are  using nodejs not browser
+
+  console.log(3+3) ;
+
+  console.log("hitesh")                               //number
+                                                    //stirng
+                                                     //bigInt         
+  let name ="sakshi"                                 //boolean => true,false
+  let age=21                                           //null,undefined
+let isLoggnedIn= false                                 //symboll=> for unique
+                                                     //obje                                                  
+    console.log(typeof name); 
