0e13e4b2a78098998f84d66a2cfc8dcdba9aeb91
Sakshinayak-2003 <nayaksakshi949@gmail.com>
2025-02-06 20:14:24 +0000

    conversion

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 14553
+let accountEmail="gakshi@gmail.com"
+var accountPassword="12345"
+accountCity="mumbai"
+let accountState;
+
+// accountId="123" not allowed as variable used constant
+
+
+accountEmail="hs@gmail.com"
+console.log(accountEmail);
+
+accountPassword="2134"
+
+accountCity="jaipur"
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score= "33abc"
+let scores =undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// conversion in number
+ let valueInNumber=Number(score)
+//  console.log(typeof valueInNumber)
+//  console.log(valueInNumber);
+
+
+ //"33" string easily converted into numbers =>33
+ // when we make "33abc" not able to convert this type of values give NAN that it is not a number
+ //true=> converts as 1; false =>0;
+
+//  CONVERSION IN BOOLEAN
+
+ let isLoggedIn = 1     //"sakshi","",
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+ //CONVERSION IN STRING
+ let someNumber= 33
+ let stringNumber =String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
