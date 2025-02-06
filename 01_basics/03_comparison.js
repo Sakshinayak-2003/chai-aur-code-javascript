c6ce75b35d00bf656380b2b0be3a4629b0867a1f
Sakshinayak-2003 <nayaksakshi949@gmail.com>
2025-02-06 20:42:01 +0000

    comparison

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+
+// problemm occurs when  not comparing same datatype*********888
+console.log("2">1);   //true
+console.log(null==0);//false
+console.log(null>0);//false
+console.log(null<0);//false
+
+
+//===
+console.log("3"==2);   // this is ggod its clean
--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -23,5 +23,44 @@ let scores =undefined
  //CONVERSION IN STRING
  let someNumber= 33
  let stringNumber =String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+
+
+//  *********************operations******************
+let value =3
+let negValue =-value
+// console.log(negValue)     //understand it deep
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 ="hello"
+let str2 =" hitesh"
+let concat=str1+str2
+console.log(concat)
+
+
+console.log("1"+2); // string and number=> convert into string
+console.log(1+"2");  //samee 
+console.log("1"+"2")
+console.log("1"+2+2); //here convert into string 
+console.log(1+2+"2");  // here first add as 3 then and add as a string that is => 32
+
+
+
+console.log(true)       // output will => true
+console.log(+true)      // output will => 1
+console.log(true+)     // errorrrrr
+console.log(+"");      //output will => 0
+
+
+// presendenceeeeeeeeeeee operator
+
+let gameCounter =100
+gameCounter++;    //++gameCounter
+console.log(gameCounter);     //output: 101
