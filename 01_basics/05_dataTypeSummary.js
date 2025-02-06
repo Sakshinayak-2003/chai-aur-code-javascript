c9b39166ab8ad08b2d8dd785b8612a0550d06948
Sakshinayak-2003 <nayaksakshi949@gmail.com>
2025-02-06 20:56:21 +0000

    DONE

---
+++ 01_basics/05_dataTypeSummary.js
@@ -0,0 +1,28 @@
+//primitive :number,string,boolean,null,undefined,symbol,BigInt
+//refernce :objects,array,functions     //js is dynmaically typed language
+
+
+ const score:number=100    ///this is strictly typed
+ const score =100
+ const scoreValue=100.3  // number dt
+
+
+ const isLOggedIn = false /// boolean
+ const hh=null
+ let userEmail;
+
+//symbol dtt
+
+ const id symbol('123')
+ const anotherId = symbol('123')
+
+
+ 
+
+ // MEMORYY STACK AND HEAP+++++++++++++++
+ //STACK IS USED IN(PRIMITIVE MEMORY), HEAP IS USE IN(NON-PRIMITIVE)
+ //IN STACK USES ALWAYS GET COPY OF VARIABLE IN MEMORY
+ //IN  HEAP MEMORY GET REFERNCE THAT IS ORIGINAL VALE ,KUCH BHI CHNAGE KAREGY VOH ORIGINALL MIEN CHNAGE HOGA..
+ 
+
+  
